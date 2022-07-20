09d51f4e24bf81de54d30fc3f3800bb34c000e75
nickspanos <nickspanos23@gmail.com>
2022-07-20 15:16:11 +0300

    Changes to source (type of data)

--- source/RLS_Estimation_Object.h
+++ source/RLS_Estimation_Object.h
@@ -16,27 +16,28 @@ namespace RLS {
 	class RLS_Estimator {
 	public:
 		typedef Mat<T> Type_Mat;
+		typedef Col<T> Type_Vec;
 
 	protected:
 		const int np; //Number of Parameters - Order of Polynomial
 		double lambda; //Forgetting Factor
 		double init_covar; //Initial Covariance (Preferably large to declare indiffirence at first iterations//
-		Type_Mat phi_matrix;
-		Type_Mat theta_matrix; //Matrix of Parameters//
+		Type_Vec phi;
+		Type_Vec theta; //Matrix of Parameters//
 		Type_Mat P_matrix; //Covariance Matrix//
-		Type_Mat K; //Gain Vector//
-		Type_Mat temp;
+		Type_Vec K; //Gain Vector//
+		Type_Vec temp;
 		unsigned long long num_update; //Number of updates//
 
 	public: //Set Defaults Values at Construction of Object//
 		RLS_Estimator(double lam, double init)
 			:np(N), lambda(1.0),
 			init_covar(init),
-			theta_matrix(Type_Mat(N,1,fill::zeros)),
+			theta(Type_Vec(N,fill::zeros)),
 			P_matrix(Type_Mat(N,N,fill::eye)),
-			phi_matrix(Type_Mat(1, N,fill::zeros)),
-			K(Type_Mat(N,1,fill::zeros)),
-			temp(Type_Mat(N,1,fill::zeros)),
+			phi(Type_Vec(N,fill::zeros)),
+			K(Type_Vec(N,fill::zeros)),
+			temp(Type_Vec(N,fill::zeros)),
 			num_update(0) {
 			setLambda(lam);
 			setCovariance(init);
@@ -47,16 +48,16 @@ namespace RLS {
 		void update_par(vec& x, T data) {
 
 			for (int i = 0; i < N; i++) {
-				phi_matrix.col(i) = x(i);
+				phi(i) = x(i);
 			}
 
-			temp = P_matrix * trans(phi_matrix);
+			temp = P_matrix * phi;
 
-			K = temp / (dot(phi_matrix, temp) + lambda);
+			K = temp / (dot(phi, temp) + lambda);
 
 			//CALCULATION OF  NEW PARAMETERS//
 
-			theta_matrix += K * (data - phi_matrix * theta_matrix); //Output is in ascending order , ie: a0 + a1*t + a2*t^2.....
+			theta += K * (data - dot(phi,theta)); //Output is in ascending order , ie: a0 + a1*t + a2*t^2.....
 
 			//CALCULATION OF NEW COVARIANCE MATRIX//
 
@@ -96,20 +97,20 @@ namespace RLS {
 		};
 
 		//"Get" Functions//
-		const Type_Mat& getEstimatedParameters() const noexcept { return theta_matrix; }
+		const Type_Vec& getEstimatedParameters() const noexcept { return theta; }
 		const Type_Mat& getCovarianceMat() const noexcept { return P_matrix; }
-		const Type_Mat& getGains() const noexcept { return K; }
+		const Type_Vec& getGains() const noexcept { return K; }
 		int getIterations() const noexcept { return num_update;  }
-		double getEstimatedOutput() const noexcept { return dot(phi_matrix, theta_matrix); }
+		double getEstimatedOutput() const noexcept { return dot(phi, theta); }
 		const double getLambda() const noexcept { return lambda; }
 		const double getCovar() const noexcept { return init_covar; }
 
 		void reset() noexcept {
-			theta_matrix = Type_Mat(1, N, fill::zeros);
+			theta = Type_Vec(N, fill::zeros);
 			P_matrix = Type_Mat(N, N,fill::eye) * init_covar;
-			K = Type_Mat(1, N, fill::zeros);
-			phi_matrix = Type_Mat(1, N, fill::zeros);
-			temp = Type_Mat(N,1,fill::zeros);
+			K = Type_Vec(N, fill::zeros);
+			phi = Type_Vec(N, fill::zeros);
+			temp = Type_Vec(N,fill::zeros);
 			num_update = 0;
 		};
 	};
@@ -123,17 +124,19 @@ namespace RLS {
 			: RLS_Estimator<T, N>(lam, init) {}
 		void update_par(T data) {
 
-			phi_matrix.col(0) = 1.;
+			phi(0) = 1.;
 			for (int i = 1; i < N; i++) {
-				phi_matrix.col(i) = phi_matrix.col(i - 1) * num_update;
+				phi(i) = phi(i - 1) * num_update;
 			}
-			temp = P_matrix * trans(phi_matrix);
-			K = temp / (dot(phi_matrix, temp) + lambda);
+	
+			temp = P_matrix * phi;
+	
+			K = temp / (dot(phi, temp) + lambda);
 
-
+	
 			//CALCULATION OF  NEW PARAMETERS//
 
-			theta_matrix += K * (data - phi_matrix * theta_matrix); //Output is in ascending order , ie: a0 + a1*t + a2*t^2.....
+			theta += K * (data - dot(theta,phi)); //Output is in ascending order , ie: a0 + a1*t + a2*t^2.....
 
 			//CALCULATION OF NEW COVARIANCE MATRIX//
 
