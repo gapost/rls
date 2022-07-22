a4e8e098dfe5e855090edd211f688e477248864e
nickspanos <nickspanos23@gmail.com>
2022-07-22 15:25:35 +0300

    Added Block check

--- examples/RLS_Estimation_Visual_studio/RLS_testing.cpp
+++ examples/RLS_Estimation_Visual_studio/RLS_testing.cpp
@@ -21,7 +21,7 @@ int main() {
 
 	double counter = 0.;
 	const double mean = 0.0;
-	const double stddev = 4;
+	const double stddev = 5;
 	std::default_random_engine generator;
 	std::normal_distribution<double> dist(mean, stddev);
 
@@ -57,7 +57,7 @@ int main() {
 
 	// Add Gaussian noise and Write Output//
 	ofstream outFile0;
-	outFile0.open("C:/Users/nicks/rls/MATLAB/TXT-Files/Test_Output.txt");
+	outFile0.open("C:/Users/nicks/rls/MATLAB/TXT-Files/PolyRLS/Test_Output.txt");
 	for (int i = 0; i < 500; i++) {
 		Y[i] = Y[i] + dist(generator);
 		outFile0 << Y[i] << " ";
@@ -121,8 +121,8 @@ int main() {
 
 	PolyRLS<double, 2> Alg_1(1., init_covar);
 
-	cout << "First Test : We expect parameters for each area to have errors as every single input is taken in regard" << endl;
-	cout << "and doesn't accurately represent the area." << endl;
+	cout << "|---------First Test : We expect parameters for each area to have errors as every single input is taken in regard---------|" << endl;
+	cout << "|---------and doesn't accurately represent the area.---------|" << endl;
 
 	for (int i = 0; i < 500; i++) {
 		cout << "Here is output: " << Y[i] << endl;
@@ -137,16 +137,16 @@ int main() {
 
 	PolyRLS<double, 2> Alg_2(0.9, init_covar);
 
-	cout << "Second Test : We expect parameters to be time-variant and change depending on the output" << endl;
-	cout << "In our case , we should see that it resembles the polynomial we put as input." << endl;
+	cout << "|---------Second Test : We expect parameters to be time-variant and change depending on the output---------|" << endl;
+	cout << "|---------In our case , we should see that it resembles the polynomial we put as input.---------|" << endl;
 
 	//Writing Data from Parameters//
 	ofstream outFile1;
 	ofstream outFile2;
 	ofstream outFile_out_2;
-	outFile1.open("C:/Users/nicks/rls/MATLAB/TXT-Files/Test2_Param_a0.txt");
-	outFile2.open("C:/Users/nicks/rls/MATLAB/TXT-Files/Test2_Param_a1.txt");
-	outFile_out_2.open("C:/Users/nicks/rls/MATLAB/TXT-Files/Test2_Est_Output.txt");
+	outFile1.open("C:/Users/nicks/rls/MATLAB/TXT-Files/PolyRLS/Test2_Param_a0.txt");
+	outFile2.open("C:/Users/nicks/rls/MATLAB/TXT-Files/PolyRLS/Test2_Param_a1.txt");
+	outFile_out_2.open("C:/Users/nicks/rls/MATLAB/TXT-Files/PolyRLS/Test2_Est_Output.txt");
 
 	for (int i = 0; i < 500; i++) {
 		cout << "Here is estimated output: " << Alg_2.getEstimatedOutput() << endl;
@@ -167,17 +167,17 @@ int main() {
 
 	PolyRLS<double, 3> Alg_3(0.9, init_covar);
 
-	cout << "Third Test : We are estimating everything as a 2nd order Polynomial"<< endl;
+	cout << "|---------Third Test : We are estimating everything as a 2nd order Polynomial---------|"<< endl;
 
 	//Writing Data from Parameters//
 	ofstream outFile3;
 	ofstream outFile4;
 	ofstream outFile5;
 	ofstream outFile_out_3;
-	outFile3.open("C:/Users/nicks/rls/MATLAB/TXT-Files/Test3_Param_a0.txt");
-	outFile4.open("C:/Users/nicks/rls/MATLAB/TXT-Files/Test3_Param_a1.txt");
-	outFile5.open("C:/Users/nicks/rls/MATLAB/TXT-Files/Test3_Param_a2.txt");
-	outFile_out_3.open("C:/Users/nicks/rls/MATLAB/TXT-Files/Test3_Est_Output.txt");
+	outFile3.open("C:/Users/nicks/rls/MATLAB/TXT-Files/PolyRLS/Test3_Param_a0.txt");
+	outFile4.open("C:/Users/nicks/rls/MATLAB/TXT-Files/PolyRLS/Test3_Param_a1.txt");
+	outFile5.open("C:/Users/nicks/rls/MATLAB/TXT-Files/PolyRLS/Test3_Param_a2.txt");
+	outFile_out_3.open("C:/Users/nicks/rls/MATLAB/TXT-Files/PolyRLS/Test3_Est_Output.txt");
 
 	for (int i = 0; i < 500; i++) {
 		cout << "Here is estimated output: " << Alg_3.getEstimatedOutput() << endl;
@@ -197,16 +197,16 @@ int main() {
 	outFile5.close();
 	outFile_out_3.close();
 
-	cout << "Takes much longer to converge with this method to the correct input,if at all. " << endl;
-	cout << "Need to look for big shifts in parameters,to validate the change,ie 50 ->6500 at that moment";
-	cout << " or 0.0002 -> -5.8";
+	cout << "|---------Takes much longer to converge with this method ---------|" << endl;
+	cout << "|---------Need to look for big shifts in parameters,to validate the change,ie 50 ->6500 at that moment---------|" << endl;;
+	cout << "|--------- or 0.0002 -> -5.8---------|";
 	cout << '\n';
 
 
 
 	//Test 4 : Testing Generalized RLS_Estimation function
-	cout << "Now we are going to test the generalized RLS function with the regressors of the" << endl;
-	cout << "polynomial to check the results" << endl;
+	cout << "|---------Now we are going to test the generalized RLS function with the regressors of the---------|" << endl;
+	cout << "|---------polynomial to check the results---------|" << endl;
 	cout << '\n';
 	vec Reg(3, fill::eye);
 	RLS_Estimator<double, 3> Test_Gen(0.9,100000.);
@@ -273,23 +273,25 @@ int main() {
 	ofstream outFileGen2;
 	ofstream outFileGen3;
 	ofstream outFile_out_Gen;
-	outFileGen1.open("C:/Users/nicks/rls/MATLAB/TXT-Files/TestGen_Param_a0.txt");
-	outFileGen2.open("C:/Users/nicks/rls/MATLAB/TXT-Files/TestGen_Param_a1.txt");
-	outFileGen3.open("C:/Users/nicks/rls/MATLAB/TXT-Files/TestGen_Param_a2.txt");
-	outFile_out_Gen.open("C:/Users/nicks/rls/MATLAB/TXT-Files/TestGen_Est_Output.txt");
+	outFileGen1.open("C:/Users/nicks/rls/MATLAB/TXT-Files/GenRLS/TestGen_Param_a0.txt");
+	outFileGen2.open("C:/Users/nicks/rls/MATLAB/TXT-Files/GenRLS/TestGen_Param_a1.txt");
+	outFileGen3.open("C:/Users/nicks/rls/MATLAB/TXT-Files/GenRLS/TestGen_Param_a2.txt");
+	outFile_out_Gen.open("C:/Users/nicks/rls/MATLAB/TXT-Files/GenRLS/TestGen_Est_Output.txt");
 
 	for (int i = 0; i < 500; i++) {
+		Reg(1) = i;
+		Reg(2) = i * i;
 		cout << "Here is estimated output: " << Test_Gen.getEstimatedOutput() << endl;
-		outFile_out_3 << Test_Gen.getEstimatedOutput() << " ";
+		outFile_out_Gen << Test_Gen.getEstimatedOutput() << " ";
 		cout << "Here is output: " << Y[i] << endl;
 		Test_Gen.update_par(Reg,Y[i]); // Update parameters in respect to Input and Output
 		cout << "Here are the estimated parameters at time " << i << " : " << endl;
 		Test_Gen.getEstimatedParameters().print(); //Print Matrix 
 		cout << '\n';
 		cout << "Here is cost : " << Test_Gen.getCost() << endl;
-		outFile3 << Test_Gen.getEstimatedParameters().row(0) << " ";
-		outFile4 << Test_Gen.getEstimatedParameters().row(1) << " ";
-		outFile5 << Test_Gen.getEstimatedParameters().row(2) << " ";
+		outFileGen1 << Test_Gen.getEstimatedParameters().row(0) << " ";
+		outFileGen2 << Test_Gen.getEstimatedParameters().row(1) << " ";
+		outFileGen3 << Test_Gen.getEstimatedParameters().row(2) << " ";
 	}
 
 	outFileGen1.close();
@@ -301,8 +303,8 @@ int main() {
 
 	//Test 6: Testing Rectangular Window Approach
 	BlockRLS<double, 3> Test_Block(6, 100000.);
-	cout << "Now we are going to test the Block RLS function with the regressors of the" << endl;
-	cout << "polynomial to check the results" << endl;
+	cout << "|---------Now we are going to test the Block RLS function with the regressors of the---------|" << endl;
+	cout << "|---------polynomial to check the results---------|" << endl;
 	cout << '\n';
 	//Do some updates to notice changes//
 	for (int i = 0; i < 20; i++) {
@@ -353,6 +355,43 @@ int main() {
 	cout << '\n';
 	cout << "Initial Covariance function : " << Test_Block.getCovar() << endl;
 	cout << '\n';
+
+
+	//Reset back to start
+	Test_Block.reset();
+	Test_Block.setLambda(0.9);
+	Test_Block.setCovariance(10000);
+
+	//Test 6: Check output of parameters from the BlockRLS class
+	ofstream outFileBlock1;
+	ofstream outFileBlock2;
+	ofstream outFileBlock3;
+	ofstream outFile_out_Block;
+	outFileBlock1.open("C:/Users/nicks/rls/MATLAB/TXT-Files/BlockRLS/TestBlock_Param_a0.txt");
+	outFileBlock2.open("C:/Users/nicks/rls/MATLAB/TXT-Files/BlockRLS/TestBlock_Param_a1.txt");
+	outFileBlock3.open("C:/Users/nicks/rls/MATLAB/TXT-Files/BlockRLS/TestBlock_Param_a2.txt");
+	outFile_out_Block.open("C:/Users/nicks/rls/MATLAB/TXT-Files/BlockRLS/TestBlock_Est_Output.txt");
+
+	for (int i = 0; i < 500; i++) {
+		Reg(1) = i;
+		Reg(2) = i * i;
+		cout << "Here is estimated output: " << Test_Block.getEstimatedOutput() << endl;
+		outFile_out_Block << Test_Block.getEstimatedOutput() << " ";
+		cout << "Here is output: " << Y[i] << endl;
+		Test_Block.update_par(Reg, Y[i]); // Update parameters in respect to Input and Output
+		cout << "Here are the estimated parameters at time " << i << " : " << endl;
+		Test_Block.getEstimatedParameters().print(); //Print Matrix 
+		cout << '\n';
+		cout << "Here is cost : " << Test_Block.getCost() << endl;
+		outFileBlock1 << Test_Block.getEstimatedParameters().row(0) << " ";
+		outFileBlock2 << Test_Block.getEstimatedParameters().row(1) << " ";
+		outFileBlock3 << Test_Block.getEstimatedParameters().row(2) << " ";
+	}
+
+	outFileBlock1.close();
+	outFileBlock2.close();
+	outFileBlock3.close();
+	outFile_out_Block.close();
 
 	return 0;
 }
