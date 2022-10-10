03731eec4c289ed54ce4550713493d02a72872dc
MatinaDt <matina.det@gmail.com>
2022-10-10 12:11:57 +0300

    changes from armadillo to eigen

--- examples/filtertest/filtertest.cpp
+++ examples/filtertest/filtertest.cpp
@@ -3,101 +3,94 @@
 #include <random>
 #include <fstream>
 #include <typeinfo>
-#include "/home/tinaa/Documents/Intership/rls/source/RLS_Estimation_Object.h"
+#include "/home/vd/rls/source/RLS_Estimation_Object.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <Eigen/Dense>
 
 using namespace RLS;
-using namespace arma;
-using namespace std;
 using namespace std;
 
-void testFF(const double* Y, int len,int factors ,double init_covar)
+void testFF(const double* Y, double ff, int len ,double init_covar)
 
 {  	
-    vec Reg(factors, fill::eye);
-
-   	RLS_Estimator<double> Test_Gen(factors,0.99,init_covar);  // (Number of factors ,Forgeting factor, Initial Covarience function)
-    // Update of Parameters with every new data// 
-	for (int i = 0; i < len; i++) { //for each sample i
-		for (int j=0; j<factors; j++){	//update polinomial factors
-			Reg(j)=pow(i,j);
-		}
-		Test_Gen.update_par(Reg,Y[i]); 
-	}
+	typedef Matrix< float, Dynamic, 1 > Vec;
+    Vec RegF=Vec::Identity(2,1);
+   	RLS_Estimator<double> Test_Gen(2, ff ,init_covar);  // (Number of factors ,Forgeting factor, Initial Covarience function)
 
 	//Print output of parameters from the generalized class in .txt file
     ofstream out;
     out.open ("build/FF.txt");
 
 	for (int i = 0; i < len; i++) {
-		for (int j=0; j<factors; j++){	//update polinomial factors
-			Reg(j)=pow(i,j);
+		RegF(0)=pow(i,0);
+		RegF(1)=pow(i,1);
+        Test_Gen.update_par(RegF,Y[i]); // Update parameters in respect to Input and Output
+		out << Test_Gen.getEstimatedOutput(RegF)<<'\t' << (Test_Gen.getEstimatedParameters()(0)) << "\t" << (Test_Gen.getEstimatedParameters()(1)) << endl;	
 		}
-        Test_Gen.update_par(Reg,Y[i]); // Update parameters in respect to Input and Output
-
-		out << Test_Gen.getEstimatedOutput(Reg)<<'\t' << (Test_Gen.getEstimatedParameters()(0)) << "\t" << (Test_Gen.getEstimatedParameters()(1)) << endl;	}
-
-    out.close();
+   		out.close();
     }
 
-void testWindow(const double* Y, int len,int factors ,double init_covar)
-    
-    {   
-        vec Reg(factors, fill::eye);
-        
-        BlockRLS<double> Test_Block(factors,1,50, init_covar);
-        	for (int i = 0; i < len; i++) { //for each sample i
-		for (int j=0; j<factors; j++){	//update polinomial factors
-			Reg(j)=pow(i,j);
-		}
-		Test_Block.update_par(Reg,Y[i]); 
-	}
+void testWindow(const double* Y, int len,double ff, int win ,double init_covar)
+{
+    typedef Matrix< float, Dynamic, 1 > Vec;
+    Vec RegW=Vec::Identity(2,1);
+    BlockRLS<double> Test_Block (2, ff , win, init_covar);
 
 	//Check output of parameters from the generalized class
- 	ofstream a0;
-    ofstream a1;
-    ofstream out;
+   	ofstream out;
+	out.open ("build/RecWin.txt");
 
-    out.open ("build/RecWin.txt");
-
-	for (int i = 0; i < len; i++) {
-		for (int j=0; j<factors; j++){	//update polinomial factors
-			Reg(j)=pow(i,j);
-		}
-        Test_Block.update_par(Reg,Y[i]); // Update parameters in respect to Input and Output
-
-		out << Test_Block.getEstimatedOutput(Reg)<<'\t' << (Test_Block.getEstimatedParameters()(0)) << "\t" << (Test_Block.getEstimatedParameters()(1)) << endl;	}
-    out.close();
-
-    }
+    for (int i = 0; i < len; i++) { //for each sample i
+		//update polinomial factors
+		//cout<<"i="<<i<<endl;
+		RegW(0)=pow(i,0);
+		RegW(1)=pow(i,1);
+		Test_Block.update_par(RegW,Y[i]); 
+		out <<Test_Block.getEstimatedOutput(RegW)<<'\t' << (Test_Block.getEstimatedParameters()(0)) << "\t" << (Test_Block.getEstimatedParameters()(1)) << endl;	
+		//cout << Test_Block.getEstimatedOutput(RegW)<<'\t' << (Test_Block.getEstimatedParameters()(0)) << "\t" << (Test_Block.getEstimatedParameters()(1)) << endl;	
+		//cout<<"\nRegW=\n"<<RegW<<endl;
+	}
+   	out.close();
+}
+   
 
 int main() {
-	
-	//Initializing parameters//
-	const int len = 1500; //Length of Array of Output/Input-Iteration function
-	double init_covar = 100.;//??
-	int factors=2;
 
-	//Generate Data for Validation
+	system("rm build/FF.txt");
+	system("rm build/Signal.txt");
+	system("rm build/RecWin.txt");
+
+	//Initializing parameters//
+	int len = 1000; //Length of Array of Output/Input-Iteration function
+	double init_covar = 80;
+	double ff = 0.97; 
+	int window_size =80;
+
+	//Initializing random signal
 	double Y[len]; 
 	ofstream SignalFile;
 	SignalFile.open ("build/Signal.txt");
-
-	for (int i = 0; i < 2*len/5; i++) {
-		Y[i]=1.0 + 0.1*(2.0*rand()/RAND_MAX-1.0);
+	for (int i = 0; i < 2*len/8; i++) {
+		Y[i]=2.0 + 0.1*(2.0*rand()/RAND_MAX-1.0);
 		SignalFile<<Y[i]<<'\n';
 	}
+	for (int i = 2*len/8; i <3*len/8; i++) {
 
-	for (int i = 2*len/5; i < len; i++) {
+		Y[i]=0.70 + 0.1*(2.0*rand()/RAND_MAX-1.0);
+		SignalFile<<Y[i]<<'\n';
+	}
+	for (int i = 3*len/8; i < len; i++) {
 
-		Y[i]=5.0 + 0.1*(2.0*rand()/RAND_MAX-1.0);
+		Y[i]=1.3 + 0.1*(2.0*rand()/RAND_MAX-1.0);
 		SignalFile<<Y[i]<<'\n';
 	}
 	SignalFile.close();
-
-    testFF( Y, len,factors, init_covar);
-	testWindow( Y, len,factors, init_covar);
-
+	
+	//Call Estimating Functions
+	testWindow( Y, len,ff, window_size , init_covar);
+	testFF( Y,ff, len, init_covar);
 return 0;
+
 }
+
